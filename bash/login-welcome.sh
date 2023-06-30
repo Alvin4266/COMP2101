efaa82aefdc0483e3e2c1ed309d6e50cf785ea9e
Alvin <200514266@student.georgianc.on.ca>
2023-06-30 15:26:01 -0400

    login-welcome.sh script

---
+++ bash/login-welcome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Modify the welcome message as desired
+welcome="Welcome to my world of scripting, $USER!"
+
+# Display the welcome message using cowsay
+cowsay "$welcome"
+
+#end of script
